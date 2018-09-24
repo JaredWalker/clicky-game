e4130b35a8b87a67e0ce046d5301c4f96d1c6306
JaredWalker <jaredstaires@gmail.com>
2018-09-23 23:45:44 -0500

    color changes

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import './Nav.css';
 const Nav = props => (
     <nav className="navbar navbar-default" >
         <ul>
-            <li className="brand">Clicky Game</li>
+            <li className="brand">American Muscle Memory Game</li>
             <li>Click on an image to begin!</li>
             <li>Current Score: {props.score} | Highest Score: {props.highScore}  </li>
         </ul>
